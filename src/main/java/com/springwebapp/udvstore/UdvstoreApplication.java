c324cc5e5b2721ffed8872466be42c9f1b8dfe4b
i.orexin <nutsmany@mail.ru>
2022-04-05 12:46:02 +0500

    Initial commit

---
+++ src/main/java/com/springwebapp/udvstore/UdvstoreApplication.java
@@ -0,0 +1,15 @@
+package com.springwebapp.udvstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdvstoreApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(UdvstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springwebapp/udvstore/UdvstoreApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springwebapp.udvstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdvstoreApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
